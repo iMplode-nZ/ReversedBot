4bfbc7c4f30cddee249af2973b12354d76064f36
iMplode nZ <rouli.freeman@gmail.com>
2020-05-27 13:06:53 -0700

    More changes...

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { prefex, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -5,14 +6,27 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
+client.on('message', messageHandler);
+
+client.login(token);
+
+function messageHandler(message) {
 	console.log(message.content);
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
+	if (message.content.startsWith(prefex)) {
+		const args = message.content.slice(prefix.length).split(' ');
+		const command = args.shift().toLowerCase();
+		console.log('Args: ' + args);
+		console.log('Command: ' + command);
+		if (command === 'args-info') {
+			if (!args.length) {
+				return message.channel.send(
+					`You didn't provide any arguments, ${message.author}!`
+				);
+			} else if (args[0] === 'foo') {
+				return message.channel.send('bar');
+			}
+
+			message.channel.send(`First argument: ${args[0]}`);
+		}
 	}
-});
-
-
-
-client.login('NzE1Mjg5NDYwNDIxNjg5NDA4.Xs7DkQ.0dJqefs5nod7k44TTFXVfIDeyHw');
+}
